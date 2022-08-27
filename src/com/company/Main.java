ebd8d6edc071e60dcd18137b0257e196c377b673
JackaBolotbekov <jackabolotbekow@gmail.com>
2022-08-27 17:13:17 +0300

    minor changesg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,22 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("Привет");
-        System.out.println("Jaiyl");
+        double[] num = {1.1, 2.2, -3.3, 4.4, -5.5, 6.6, -7.7, 8.8, -9.9, 10.10, -11.11, 12.12, -13.13, 14.14, -15.15};
+        double result = 0;
+        int a = 0;
+        boolean b = false;
+        for (double names : num) {
+            if (names > 0) {
+                b = true;
+
+            } else if (b) {
+                result = +names;
+                a++;
+            }
+        }
+        System.out.println(result);
+        System.out.println(a);
+        System.out.println("arithematic mean" + result / a);
+
     }
-    
 }
