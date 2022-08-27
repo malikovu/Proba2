5f9deea9153f4d0a086a6688124af35f1cc0c296
JackaBolotbekov <jackabolotbekow@gmail.com>
2022-08-27 17:13:51 +0300

    minor changesg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         double[] num = {1.1, 2.2, -3.3, 4.4, -5.5, 6.6, -7.7, 8.8, -9.9, 10.10, -11.11, 12.12, -13.13, 14.14, -15.15};
@@ -7,6 +9,7 @@ public class Main {
         int a = 0;
         boolean b = false;
         for (double names : num) {
+
             if (names > 0) {
                 b = true;
 
@@ -19,5 +22,23 @@ public class Main {
         System.out.println(a);
         System.out.println("arithematic mean" + result / a);
 
-    }
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < num.length; i++) {
+                if (num[i] < num[i - 1]) {
+                    double temp = num[i];
+                    num[i] = num[i - 1];
+                    num[i - 1] = temp;
+                    isSorted = false;
+                }
+            }
+            for (int i = 0; i < num.length; i++) {
+                if (i > 0) {
+
+                }
+            }
+            System.out.println(Arrays.toString(num));
+        }
+        }
 }
